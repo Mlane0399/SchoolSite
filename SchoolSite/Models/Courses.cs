23d33b5831914697d3e6f41d1f8f1e6b0454bee5
mlane0399 <lane.293@gmail.com>
2017-06-23 05:41:26 -0400

    new commit

---
+++ SchoolSite/Models/Courses.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SchoolSite.Models
+{
+    public class Courses
+    {
+        
+            [Key]
+        public int ID { get; set; }
+
+        [Display(Name = "Course Name")]
+        public string CourseName { get; set; }
+        public double Credits { get; set; }
+
+        [ForeignKey("Enrollment")]
+        [Display(Name = "Class Number")]
+        public int EnrollmentID { get; set; }
+        public virtual ICollection <Enrollment> Enrollment { get; set; }
+
+        
+    }
+}
---
+++ SchoolSite/Models/Enrollment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SchoolSite.Models
+{
+    public class Enrollment
+    {
+        [Key]
+        public int ID { get; set; }
+        public double GPA { get; set; }
+
+        [Display(Name = "Enrolled?")]
+        public bool isEnrolled { get; set; }
+
+        [ForeignKey("Courses")]
+        [Display(Name = "Course Name")]
+        public int CoursesID { get; set; }
+        public virtual Courses Courses { get; set; }
+
+        [ForeignKey("Students")]
+        [Display(Name = "Student Name")]
+        public int StudentsID { get; set; }
+        public virtual Students Students { get; set; }
+    }
+}
